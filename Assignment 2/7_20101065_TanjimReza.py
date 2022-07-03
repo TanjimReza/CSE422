4781fffa141e321d696832c4a41014a83eebc22d
Tanjim Reza <35164789+TanjimReza@users.noreply.github.com>
2022-07-03 23:43:41 +0600

    DONE

--- Assignment 2/7_20101065_TanjimReza.py
+++ Assignment 2/7_20101065_TanjimReza.py
@@ -1,9 +1,20 @@
 
+#!------------------------------------ !#
+"""
+    Name          : Tanjim Reza
+    Student ID    : 20101065
+    Course        : CSE422 - Artificial Intelligence
+    Section       : 07
+    Lab Assignment: 2
+
+"""
+#!------------------------------------ !#
+
 import time
 import random
 from pprint import pprint 
-start_time = time.time()
-with open('./1.txt', 'r') as f:
+# start_time = time.time()
+with open('input.txt', 'r') as f:
     data = f.read()
     data = data.split('\n')
     batsman = int(data[0].split(',')[0].split(' ')[0])
@@ -51,9 +62,9 @@ def generate_model(batsman):
         new_model.append(
             random.randint(0,1)
         )
-    return new_model    
+    return new_model 
 
-population_count = 100
+population_count = 1000
 population = []
 iteration = 1000
 
@@ -66,7 +77,8 @@ for i in range(population_count):
 # model = [1,0,1,0,1,1,1,0]
 # population.append(model)
 print("Population:", len(population))
-print("--->>> START <<<---")
+print("Iterations:", iteration)
+# print("--->>> START <<<---")
 # pprint(population)
 DO_WE_ITERATE = True
 SOLUTION_FOUND = False
@@ -79,21 +91,20 @@ for i in range(iteration):
         #sort the population based on fitness
         
         if target_found(model): #! IF TARGET IS FOUND
-            print("------------------")
-            print(f"SOLUTION MODEL: {model}")
-            print("------------------")
+            # print("------------------")
+            # print(f"SOLUTION MODEL: {model}")
+            # print("------------------")
             DO_WE_ITERATE = False
             SOLUTION_FOUND = True
             SOLUTION_MODEL = model
             break
     if DO_WE_ITERATE:
         new_population = []
+        #! SORTING THE POPULATION BASED ON FITNESS
         population.sort(key=lambda x: fitness(x, target), reverse=True)
-        
         for i in range(population_count):
             offspring_1, offspring_2 = crossover(population[0], population[1])
-            
-            #mutate the offspring with a probability of 0.5
+            #! MUTATION PROBABILITY 50%
             probability = random.randint(0,1)
             if probability == 1:
                 offspring_1 = mutate(offspring_1)
@@ -103,9 +114,10 @@ for i in range(iteration):
         population = new_population
     if DO_WE_ITERATE == False:
         break
-print("->>>> OUT OF LOOP <<<<<-")
+# print("->>>> OUT OF LOOP <<<<<-")
 
 if SOLUTION_FOUND: 
+    print("All Batsman:",batsman_names)
     print("Solution Model: ", SOLUTION_MODEL)
     #batsman name from solution model 
     batsman_name = []
@@ -113,9 +125,9 @@ if SOLUTION_FOUND:
         if SOLUTION_MODEL[i] == 1:
             batsman_name.append(batsman_names[i])
                 
-    print("Batsman Name: ", batsman_name)
+    print("Selected Batsman", batsman_name)
 else: 
     print("-1")
 
-end_time = time.time()
-print("(t) Runtime:", end_time - start_time)
+# end_time = time.time()
+# print("(t) Runtime:", end_time - start_time)
