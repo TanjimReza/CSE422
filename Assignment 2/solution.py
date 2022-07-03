fc5842e3bcc53489b05e9994d3a843dc9091d706
Tanjim Reza <35164789+TanjimReza@users.noreply.github.com>
2022-07-03 23:11:42 +0600

    OK

--- Assignment 2/solution.py
+++ Assignment 2/solution.py
@@ -1,9 +1,8 @@
-# model input from text file
 
+import time
 import random
-from pprint import pprint
-import re
-
+from pprint import pprint 
+start_time = time.time()
 with open('./1.txt', 'r') as f:
     data = f.read()
     data = data.split('\n')
@@ -19,24 +18,24 @@ def fitness(model, target):
     for i in range(len(model)):
         if model[i] == 1:
             score += runs[i]
-    print("(f) M_Score:",score)
+    # print("(f) M_Score:",score)
     fit = score - target
     
     if fit == 0: 
         return 20101065
     else: 
-        return abs(1/fit)
+        return abs(fit)
 
-def do_we_continue(model):
+def target_found(model):
     if fitness(model, target) == 20101065:
-        return False
+        return True
     else:
-        return True    
+        return False    
 
 def mutate(model):
     index = random.randint(0, len(model)-1)
     model[index] = 1 - model[index]
-    print("Mutation Point:", index)
+    # print("Mutation Point:", index)
     return model
 
 def crossover(model1, model2):
@@ -46,10 +45,6 @@ def crossover(model1, model2):
     return new_model_one, new_model_two
 
 
-
-population_count = 2
-population = []
-iteration = 10
 def generate_model(batsman):
     new_model = []
     for i in range(batsman):
@@ -58,60 +53,69 @@ def generate_model(batsman):
         )
     return new_model    
 
+population_count = 100
+population = []
+iteration = 1000
+
 for i in range(population_count):
     population.append(
         generate_model(batsman)
     ) 
 
 
-model = [1,0,1,0,1,1,1,0]
-population.append(model)
+# model = [1,0,1,0,1,1,1,0]
+# population.append(model)
 print("Population:", len(population))
 print("--->>> START <<<---")
 # pprint(population)
 DO_WE_ITERATE = True
+SOLUTION_FOUND = False
+SOLUTION_MODEL = None
 for i in range(iteration):
     for model in population:
-        print("Model:",model)
+        # print("Model:",model)
         fitness_of_current_model = fitness(model, target)
-        #fitness to 4 decimal places
-        print("Fitness of current model:", round(fitness_of_current_model,6))
+        # print("Fitness of current model:", round(fitness_of_current_model,6))
         #sort the population based on fitness
         
-        
-         
-        if fitness_of_current_model == 20101065:
+        if target_found(model): #! IF TARGET IS FOUND
             print("------------------")
-            print("Solution found")
             print(f"SOLUTION MODEL: {model}")
             print("------------------")
             DO_WE_ITERATE = False
+            SOLUTION_FOUND = True
+            SOLUTION_MODEL = model
             break
-        else:
-            print("Else")
-            population.sort(key=lambda x: fitness(x, target), reverse=True)
-            print("Sorted Population:")
-            for model in population:
-                print(f"-Model: {model}, Score: {fitness(model, target)}")
-            print("END: Sorted Population:")
-            
+    if DO_WE_ITERATE:
+        new_population = []
+        population.sort(key=lambda x: fitness(x, target), reverse=True)
+        
+        for i in range(population_count):
             offspring_1, offspring_2 = crossover(population[0], population[1])
             
-            
-            # crossover_model = crossover(model, population[random.randint(0, len(population)-1)])
-            # model = mutate(model)
-            # population.append(model)
-            # population.sort(reverse=True)
-            # population.pop()
-            # print("Population:", len(population))
-            # pprint(population)
-            # print("\n")
-            continue
+            #mutate the offspring with a probability of 0.5
+            probability = random.randint(0,1)
+            if probability == 1:
+                offspring_1 = mutate(offspring_1)
+                offspring_2 = mutate(offspring_2)
+            new_population.append(offspring_1)
+            new_population.append(offspring_2)
+        population = new_population
     if DO_WE_ITERATE == False:
         break
 print("->>>> OUT OF LOOP <<<<<-")
-# fits = fitness(model, target)
-# print("Fitness:", fits)
-# mutation = mutate(model)
-# print("Mutation:", mutation)
 
+if SOLUTION_FOUND: 
+    print("Solution Model: ", SOLUTION_MODEL)
+    #batsman name from solution model 
+    batsman_name = []
+    for i in range(len(SOLUTION_MODEL)): 
+        if SOLUTION_MODEL[i] == 1:
+            batsman_name.append(batsman_names[i])
+                
+    print("Batsman Name: ", batsman_name)
+else: 
+    print("-1")
+
+end_time = time.time()
+print("(t) Runtime:", end_time - start_time)
